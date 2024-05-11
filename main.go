08a2c7cc66a2e643e63b1792397a79bccd48e3da
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2024-05-11 00:32:58 -0400

    Added clearning screen after app is closed

--- main.go
+++ main.go
@@ -3,8 +3,15 @@ Copyright © 2024 Samuel Dasilva
 */
 package main
 
-import "github.com/SamD2021/boba-break/cmd"
+import (
+	"github.com/SamD2021/boba-break/cmd"
+	"os"
+)
 
 func main() {
 	cmd.Execute()
+	_, err := os.Stdout.Write([]byte{0x1B, 0x5B, 0x33, 0x3B, 0x4A, 0x1B, 0x5B, 0x48, 0x1B, 0x5B, 0x32, 0x4A})
+	if err != nil {
+		return
+	}
 }
